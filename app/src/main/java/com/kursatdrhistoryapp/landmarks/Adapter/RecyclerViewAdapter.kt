49fccae3861fa995ba62e2c7be95aa4b84cfa4d4
DrHistory <kursatkumsuz@gmail.com>
2022-08-06 16:09:17 +0300

    arraylist changed with list

--- app/src/main/java/com/kursatdrhistoryapp/landmarks/Adapter/RecyclerViewAdapter.kt
+++ app/src/main/java/com/kursatdrhistoryapp/landmarks/Adapter/RecyclerViewAdapter.kt
@@ -8,31 +8,26 @@ import com.kursatdrhistoryapp.landmarks.view.InfoActivity
 import com.kursatdrhistoryapp.landmarks.model.Landmarks
 import com.kursatdrhistoryapp.landmarks.databinding.RowBinding
 
-class RecyclerViewAdapter (val landmarklist : ArrayList<Landmarks>) : RecyclerView.Adapter<RecyclerViewAdapter.LandmarkHolder>() {
+class RecyclerViewAdapter(val landmarklist: List<Landmarks>) :
+    RecyclerView.Adapter<RecyclerViewAdapter.LandmarkHolder>() {
 
-    class LandmarkHolder (val binding : RowBinding) : RecyclerView.ViewHolder(binding.root)
-    {
+    class LandmarkHolder(val binding: RowBinding) : RecyclerView.ViewHolder(binding.root) {
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LandmarkHolder {
-      val binding = RowBinding.inflate(LayoutInflater.from(parent.context) , parent , false)
-      return LandmarkHolder(binding)
+        val binding = RowBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return LandmarkHolder(binding)
     }
 
     override fun onBindViewHolder(holder: LandmarkHolder, position: Int) {
-        holder.binding.rowNameText.text = landmarklist.get(position).name
-        holder.binding.rowCitytext.text = landmarklist.get(position).city
-        holder.binding.rowImageview.setImageResource(landmarklist.get(position).main_image)
-
-        holder.itemView.setOnClickListener {
-            val intent = Intent(holder.itemView.context , InfoActivity::class.java)
-            intent.putExtra("landmark", landmarklist.get(position))
-            holder.itemView.context.startActivity(intent)
-        }
+        holder.binding.rowNameText.text = landmarklist[position].name
+        holder.binding.rowCitytext.text = landmarklist[position].city
+        holder.binding.rowImageview.setImageResource(landmarklist[position].image)
     }
 
     override fun getItemCount(): Int {
         return landmarklist.size
     }
+
 }
